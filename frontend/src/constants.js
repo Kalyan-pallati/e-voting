11731d4c57bb73d774d467e75c0602af24e5346e
Kalyan-pallati <pallatikalyansai@gmail.com>
2026-02-23 16:03:03 +0530

    Fixed typo mistakes

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 import VotingSystemABI from "./artifacts/VotingSystem.json";
 
-export const CONTRACT_ADDRESS = "0x52b0809d075E11611B432CDb1C46a7e421cB4864"; 
+export const CONTRACT_ADDRESS = "YOUR_CONTRACT_ADDRESS"; 
 
 export const CONTRACT_ABI = VotingSystemABI.abi;
